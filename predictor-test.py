e8c7d477ba938b5fe341523c7b656e52aa67fca8
Eli Maor <eli.maor@gmail.com>
2023-04-07 22:55:37 +0300

    updare predictor-test.py

--- predictor-test.py
+++ predictor-test.py
@@ -1,6 +1,7 @@
 from IPython.display import display, HTML
+
 display(HTML(
-"""
+    """
 <a target="_blank" href="https://colab.research.google.com/github/facebookresearch/segment-anything/blob/main/notebooks/predictor_example.ipynb">
   <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/>
 </a>
@@ -9,69 +10,71 @@ display(HTML(
 
 import torch
 import torchvision
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import os, sys
+
 print("PyTorch version:", torch.__version__)
 print("Torchvision version:", torchvision.__version__)
 print("CUDA is available:", torch.cuda.is_available())
-import os, sys
 
 torch.cuda.empty_cache()
 torch.cuda.memory_summary(device=None, abbreviated=False)
 
-dirname = "images" 
+dirname = "images"
 if os.path.isdir(dirname) == False:
-    #Create the Directiory
+    # Create the Directiory
     os.mkdir(dirname)
-    #Print success massage
-    print ("The directory is created")
+    # Print success massage
+    print("The directory is created")
 else:
-    #Print the massage if the directory exists
-    print ("The directory already exists.")
-import wget, shutil 
+    # Print the massage if the directory exists
+    print("The directory already exists.")
+import wget, shutil
 
-if os.path.isfile("images/truck.jpg") == False:
-   wget.download("https://raw.githubusercontent.com/facebookresearch/segment-anything/main/notebooks/images/truck.jpg") 
-   shutil.move("truck.jpg", "images")
-if os.path.isfile("images/groceries.jpg") == False:
-   wget.download("https://raw.githubusercontent.com/facebookresearch/segment-anything/main/notebooks/images/groceries.jpg")
-   shutil.move("groceries.jpg", "images")
-
-import numpy as np
-import matplotlib.pyplot as plt
-import cv2
+if not os.path.isfile("images/truck.jpg"):
+    wget.download("https://raw.githubusercontent.com/facebookresearch/segment-anything/main/notebooks/images/truck.jpg")
+    shutil.move("truck.jpg", "images")
+if not os.path.isfile("images/groceries.jpg"):
+    wget.download(
+        "https://raw.githubusercontent.com/facebookresearch/segment-anything/main/notebooks/images/groceries.jpg")
+    shutil.move("groceries.jpg", "images")
 
 def show_mask(mask, ax, random_color=False):
     if random_color:
         color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
     else:
-        color = np.array([30/255, 144/255, 255/255, 0.6])
+        color = np.array([30 / 255, 144 / 255, 255 / 255, 0.6])
     h, w = mask.shape[-2:]
     mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
     ax.imshow(mask_image)
-    
+
 def show_points(coords, labels, ax, marker_size=375):
-    pos_points = coords[labels==1]
-    neg_points = coords[labels==0]
-    ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
-    ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)   
-    
+    pos_points = coords[labels == 1]
+    neg_points = coords[labels == 0]
+    ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='*', s=marker_size, edgecolor='white',
+               linewidth=1.25)
+    ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white',
+               linewidth=1.25)
+
 def show_box(box, ax):
     x0, y0 = box[0], box[1]
     w, h = box[2] - box[0], box[3] - box[1]
-    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0,0,0,0), lw=2))
+    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0, 0, 0, 0), lw=2))
 
 image = cv2.imread('images/truck.jpg')
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-plt.figure(figsize=(10,10))
+plt.figure(figsize=(10, 10))
 plt.imshow(image)
 plt.axis('on')
 plt.show()
 
 sam_checkpoint = "models/sam_vit_h_4b8939.pth"
-device = "cpu" #"cuda"
+device = "cpu"  # "cuda"
 model_type = "default"
 
-import sys
 sys.path.append("..")
 from segment_anything import sam_model_registry, SamPredictor
 
@@ -85,7 +88,7 @@ predictor.set_image(image)
 input_point = np.array([[500, 375]])
 input_label = np.array([1])
 
-plt.figure(figsize=(10,10))
+plt.figure(figsize=(10, 10))
 plt.imshow(image)
 show_points(input_point, input_label, plt.gca())
 plt.axis('on')
@@ -97,17 +100,16 @@ masks, scores, logits = predictor.predict(
     multimask_output=True,
 )
 
-masks.shape  # (number_of_masks) x H x W
+# masks.shape  # (number_of_masks) x H x W
 
 for i, (mask, score) in enumerate(zip(masks, scores)):
-    plt.figure(figsize=(10,10))
+    plt.figure(figsize=(10, 10))
     plt.imshow(image)
     show_mask(mask, plt.gca())
     show_points(input_point, input_label, plt.gca())
-    plt.title(f"Mask {i+1}, Score: {score:.3f}", fontsize=18)
+    plt.title(f"Mask {i + 1}, Score: {score:.3f}", fontsize=18)
     plt.axis('off')
-    plt.show()  
-
+    plt.show()
 
 input_point = np.array([[500, 375], [1125, 625]])
 input_label = np.array([1, 1])
@@ -121,9 +123,9 @@ masks, _, _ = predictor.predict(
     multimask_output=False,
 )
 
-masks.shape
+# masks.shape
 
-plt.figure(figsize=(10,10))
+plt.figure(figsize=(10, 10))
 plt.imshow(image)
 show_mask(masks, plt.gca())
 show_points(input_point, input_label, plt.gca())
@@ -199,7 +201,7 @@ masks, _, _ = predictor.predict_torch(
     multimask_output=False,
 )
 
-masks.shape  # (batch_size) x (num_predicted_masks_per_input) x H x W
+# masks.shape  # (batch_size) x (num_predicted_masks_per_input) x H x W
 
 plt.figure(figsize=(10, 10))
 plt.imshow(image)
@@ -236,32 +238,33 @@ image3_boxes = torch.tensor([
     [580, 170, 640, 350],
 ], device=sam.device)
 
-
 from segment_anything.utils.transforms import ResizeLongestSide
+
 resize_transform = ResizeLongestSide(sam.image_encoder.img_size)
 
 def prepare_image(image, transform, device):
     image = transform.apply_image(image)
-    image = torch.as_tensor(image, device=device.device) 
+    image = torch.as_tensor(image, device=device.device)
     return image.permute(2, 0, 1).contiguous()
 
+
 batched_input = [
-     {
-         'image': prepare_image(image1, resize_transform, sam),
-         'boxes': resize_transform.apply_boxes_torch(image1_boxes, image1.shape[:2]),
-         'original_size': image1.shape[:2]
-     },
-     {
-         'image': prepare_image(image2, resize_transform, sam),
-         'boxes': resize_transform.apply_boxes_torch(image2_boxes, image2.shape[:2]),
-         'original_size': image2.shape[:2]
-     },
-     {
-         'image': prepare_image(image3, resize_transform, sam),
-         'boxes': resize_transform.apply_boxes_torch(image3_boxes, image3.shape[:2]),
-         'original_size': image3.shape[:2]
-    
-     }
+    {
+        'image': prepare_image(image1, resize_transform, sam),
+        'boxes': resize_transform.apply_boxes_torch(image1_boxes, image1.shape[:2]),
+        'original_size': image1.shape[:2]
+    },
+    {
+        'image': prepare_image(image2, resize_transform, sam),
+        'boxes': resize_transform.apply_boxes_torch(image2_boxes, image2.shape[:2]),
+        'original_size': image2.shape[:2]
+    },
+    {
+        'image': prepare_image(image3, resize_transform, sam),
+        'boxes': resize_transform.apply_boxes_torch(image3_boxes, image3.shape[:2]),
+        'original_size': image3.shape[:2]
+
+    }
 ]
 
 batched_output = sam(batched_input, multimask_output=False)
@@ -293,5 +296,3 @@ ax[1].axis('off')
 
 plt.tight_layout()
 plt.show()
-
-
